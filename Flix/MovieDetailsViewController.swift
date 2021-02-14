da82164558cbbc9ae02f6cac8f8cfb9a57c74c60
Melanie Chan <53189099+melanieChan@users.noreply.github.com>
2021-02-13 16:15:17 -0800

    displayed movie details

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
     
+    @IBOutlet weak var backdropImage: UIImageView!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+
     // movie whose details will be displayed
     var movie: [String: Any]!
 
@@ -17,6 +23,23 @@ class MovieDetailsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         print(movie["title"])
+        
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        
+        // Images
+        let baseUrl = "https://image.tmdb.org/t/p/w185" // base url of images from this API
+        
+        // poster
+        let posterPath = movie["poster_path"] as! String    // string of url that leads to specific poster
+        let posterUrl = URL(string: baseUrl + posterPath)   // combines base url & poster path into complete url
+        posterImage.af_setImage(withURL: posterUrl!)    // set image
+
+        // backdrop
+        let backdropPath = movie["backdrop_path"] as! String    // string of url that leads to specific poster
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)   // combines base url & poster path into complete url
+        backdropImage.af_setImage(withURL: backdropUrl!)    // set image
+
     }
     
 
