986b8f924daded36e154d228fa751bd1d273fe52
Melanie Chan <53189099+melanieChan@users.noreply.github.com>
2021-02-13 19:47:40 -0800

    added network request
    to get similar movies from api

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class MovieGridViewController: UIViewController {
+    
+    // store data from API
+    var movies = [[String: Any]]() // array of dictionaries
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Get superhero movies
+        // network request to get data from API
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+//            print(dataDictionary)
+            
+            // get data from dictionary and store into class variable
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+            
+//            print(self.movies)
+
+           }
+        }
+        task.resume()
+
     }
     
 
